d645c4a1e62a73746556cce89d6f9411bf717575
19950812amZ <ozerov.webmaster496@gmail.com>
2020-10-25 22:50:04 +0200

    build clients block || adapted clients blocks || add transition for theme switcher

--- dist/js/script.js
+++ dist/js/script.js
@@ -84,11 +84,13 @@ document.addEventListener('scroll', (e) => {
     function parallaxVideo(){
         const block1 = document.querySelector('.block__mod-1');
         const block4 = document.querySelector('.block__mod-4');
+        const block5 = document.querySelector('.block__mod-5');
         const screenHeight = window.innerHeight || document.documentElement.clientHeight;
         const screenWidth = window.innerWidth || document.documentElement.clientWidth;
 
         let top = block1.getBoundingClientRect().top;
         let top2 = block4.getBoundingClientRect().top;
+        let top3 = block5.getBoundingClientRect().top;
 
         function parallaxDown(element){
             const elem = document.querySelector(element).getBoundingClientRect().top;
@@ -112,21 +114,23 @@ document.addEventListener('scroll', (e) => {
 
             document.querySelector(element).style.cssText = `transform: translate3d(0px, ${e}px, 0px);`
         }
-        function noTransform(element,element2){
+        function noTransform(element){
             document.querySelector(element).classList.add('m-video-trans-off')
-            document.querySelector(element2).classList.add('m-video-trans-off')
         }
 
-        if(top <= screenHeight / 1.2){
-            noTransform('.g-project-elem-1', '.g-project-elem-2')
-        }
+
         if(top <= screenHeight && top >= 0 && screenWidth >= 1000){
+            noTransform('.g-project-elem-1')
+            noTransform('.g-project-elem-2')
             parallaxDown('.block__mod-1');
             parallaxTop('.block__mod-2');
         }
         if(top2 <= screenHeight && top2 >= 0 && screenWidth >= 1000){
             parallaxTop('.block__mod-3');
             parallaxDown('.block__mod-4');
+        }
+        if(top3 <= screenHeight / 1.5){
+            noTransform('.g-project-elem-3')
         }
     }
     parallaxVideo();
